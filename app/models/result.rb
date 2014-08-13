b9ed51cfe9688f881afa02b8404eac172a25ecb9
Logan Yu <yu.logan@gmail.com>
2014-08-13 13:56:49 -0400

    added associations

--- app/models/result.rb
+++ app/models/result.rb
@@ -1,2 +1,5 @@
 class Result < ActiveRecord::Base
+
+  belongs_to :runner, inverse_of: :results
+  belongs_to :race, inverse_of: :results
 end
--- app/models/runner.rb
+++ app/models/runner.rb
@@ -1,2 +1,5 @@
 class Runner < ActiveRecord::Base
+  
+  has_many :results, inverse_of: :runner
+  has_many :races, through: :results
 end
