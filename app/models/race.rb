35dbf246cd61e01b73c5275b86dee8ce93858932
Logan Yu <yu.logan@gmail.com>
2014-08-13 23:14:08 -0400

    more bootstrap

--- app/models/race.rb
+++ app/models/race.rb
@@ -1,5 +1,2 @@
 class Race < ActiveRecord::Base
-  
-  has_many :race_entries, inverse_of: :race
-  has_many :runners, through: :results
 end
--- app/models/runner.rb
+++ app/models/runner.rb
@@ -1,5 +1,2 @@
 class Runner < ActiveRecord::Base
-  
-  has_many :results, inverse_of: :runner
-  has_many :races, through: :results
 end
