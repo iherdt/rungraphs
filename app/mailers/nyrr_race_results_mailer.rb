9891ca7c32f51791c3e03a3b79d6d558a81266b7
Logan Yu <yu.logan@gmail.com>
2016-07-31 00:18:01 -0400

    easier to set mailer variables

--- app/mailers/nyrr_race_results_mailer.rb
+++ app/mailers/nyrr_race_results_mailer.rb
@@ -2,8 +2,8 @@ class NyrrRaceResultsMailer < ApplicationMailer
   REPORT_EMAIL = "rungraphs-reports@googlegroups.com"
   NBR_TEAM_NAME = "nbr"
 
-  def nyrr_results_report(report_email = REPORT_EMAIL)
-    nyrr_race_results_provider = Rungraphs::Analytics::NYRRRaceResultsAnalyticsProvider.new(Time.now.at_midnight - 1.week, Time.now.at_midnight)
+  def nyrr_results_report(report_email = REPORT_EMAIL, start_time = Time.now.at_midnight - 1.week, end_time = Time.now.at_midnight)
+    nyrr_race_results_provider = Rungraphs::Analytics::NYRRRaceResultsAnalyticsProvider.new(start_time, end_time)
     @races = nyrr_race_results_provider.get_local_competitive_race_results
     if @races.length == 0
       puts "No results. Bailing on email."
@@ -13,8 +13,8 @@ class NyrrRaceResultsMailer < ApplicationMailer
     end
   end
 
-  def nbr_results_report(report_email = REPORT_EMAIL)
-    nyrr_race_results_provider = Rungraphs::Analytics::NYRRRaceResultsAnalyticsProvider.new(Time.now.at_midnight, Time.now.at_midnight)
+  def nbr_results_report(report_email = REPORT_EMAIL, start_time = Time.now.at_midnight, end_time = Time.now.at_midnight)
+    nyrr_race_results_provider = Rungraphs::Analytics::NYRRRaceResultsAnalyticsProvider.new(start_time, end_time)
     @races = nyrr_race_results_provider.get_team_race_results(NBR_TEAM_NAME)
     if @races.length == 0
       puts "No results. Bailing on email."
